19b3f4766db79675f9e55cdbac427552e289015a
Louis Hildebrand <louis.hildebrand@mcgill.ca>
2023-02-10 16:03:52 -0500

    Add JPA annotations

--- EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/model/Person.java
+++ EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/model/Person.java
@@ -1,7 +1,15 @@
 package ca.mcgill.ecse321.eventregistration.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Person {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
 	
 	private String name;
--- EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/model/Registration.java
+++ EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/model/Registration.java
@@ -1,25 +1,59 @@
 package ca.mcgill.ecse321.eventregistration.model;
 
-public class Registration {
-	
-	private Event event;
+import java.io.Serializable;
+import java.util.Objects;
 
-	private Person person;
-	
-	public Event getEvent() {
-		return event;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Registration {
+
+	@EmbeddedId
+	private RegistrationId id;
+
+	public static class RegistrationId implements Serializable {
+
+		private static final long serialVersionUID = 1L;
+
+		@ManyToOne
+		private Event event;
+
+		@ManyToOne
+		private Person person;
+
+		public Event getEvent() {
+			return event;
+		}
+
+		public Person getPerson() {
+			return person;
+		}
+
+		public void setEvent(Event event) {
+			this.event = event;
+		}
+
+		public void setPerson(Person person) {
+			this.person = person;
+		}
+
+		@Override
+		public boolean equals(Object o) {
+			if (!(o instanceof RegistrationId)) {
+				return false;
+			}
+
+			RegistrationId r = (RegistrationId) o;
+			return this.event.getId() == r.event.getId() && this.person.getId() == r.person.getId();
+		}
+		
+		@Override
+		public int hashCode() {
+			return Objects.hash(this.event, this.person);
+		}
+
 	}
-	
-	public Person getPerson() {
-		return person;
-	}
-	
-	public void setEvent(Event event) {
-		this.event = event;
-	}
-	
-	public void setPerson(Person person) {
-		this.person = person;
-	}
-	
+
 }
