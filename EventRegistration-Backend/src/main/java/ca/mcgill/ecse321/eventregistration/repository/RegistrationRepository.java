649a327e68e127070ba10d55723306d61978ab9d
Louis Hildebrand <louis.hildebrand@mcgill.ca>
2023-02-17 10:01:32 -0500

    Define repository interfaces

---
+++ EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/repository/RegistrationRepository.java
@@ -0,0 +1,12 @@
+package ca.mcgill.ecse321.eventregistration.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import ca.mcgill.ecse321.eventregistration.model.Registration;
+import ca.mcgill.ecse321.eventregistration.model.Registration.RegistrationId;
+
+public interface RegistrationRepository extends CrudRepository<Registration, RegistrationId> {
+
+	public Registration findRepositoryById(RegistrationId id);
+	
+}
