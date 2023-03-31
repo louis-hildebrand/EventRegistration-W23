0dfacacd0691b8c2e6c436ca1830712323bb4e06
Louis Hildebrand <louis.hildebrand@mcgill.ca>
2023-03-31 14:28:45 -0400

    Allow cross-origin requests in backend

--- EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/controller/PersonController.java
+++ EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/controller/PersonController.java
@@ -6,6 +6,7 @@ import java.util.stream.StreamSupport;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +20,7 @@ import ca.mcgill.ecse321.eventregistration.service.PersonService;
 import jakarta.validation.Valid;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:8087")
 public class PersonController {
 
 	@Autowired
