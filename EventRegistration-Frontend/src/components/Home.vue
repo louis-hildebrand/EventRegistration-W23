4cff660e9d76415e741e7c526ba6dab7b7baec8d
Louis Hildebrand <louis.hildebrand@mcgill.ca>
2023-03-31 14:13:15 -0400

    Make user list dynamic and allow creating users

--- EventRegistration-Frontend/src/components/Home.vue
+++ EventRegistration-Frontend/src/components/Home.vue
@@ -6,16 +6,63 @@
         <th>Name</th>
         <th>Creation date</th>
       </tr>
-      <tr>
+      <tr v-for="p in persons">
+        <td>{{ p.name }}</td>
+        <td>{{ p.creationDate }}</td>
+      </tr>
+      <!-- <tr>
         <td>John</td>
         <td>2023/03/31</td>
-      </tr>
+      </tr> -->
     </table>
+    <h2>New User</h2>
+    <input type="text" placeholder="Name" v-model="newPersonName">
+    <input type="password" placeholder="Password" v-model="newPersonPassword">
+    <button v-bind:disabled="createUserButtonDisabled" @click="createUser()">Create</button>
+    <p class="error">{{ errorMsg }}</p>
   </div>
 </template>
 
 <script>
-
+export default {
+  name: 'Home',
+  data() {
+    return {
+      persons: [
+        {name: 'John', creationDate: '2023/03/31'},
+        {name: 'Alice', creationDate: '2022/12/31'},
+      ],
+      newPersonName: '',
+      newPersonPassword: '',
+      errorMsg: '',
+    };
+  },
+  methods: {
+    createUser() {
+      if (this.newPersonName === 'C3PO') {
+        this.errorMsg = 'Droids are not allowed.';
+        return;
+      }
+      
+      const now = new Date();
+      const year = now.getFullYear();
+      const month = String(now.getMonth() + 1).padStart(2, 0);
+      const day = String(now.getDate()).padStart(2, 0);
+      const dateStr = `${year}/${month}/${day}`;
+      const person = {name: this.newPersonName, creationDate: dateStr}
+      this.persons.push(person);
+      
+      this.newPersonName = '';
+      this.newPersonPassword = '';
+      this.errorMsg = '';
+    }
+  },
+  computed: {
+    createUserButtonDisabled() {
+      return !this.newPersonName.trim() || !this.newPersonPassword.trim();
+    }
+  }
+}
 </script>
 
 <style>
@@ -31,7 +78,7 @@ td, th {
   align-items: stretch;
 }
 
-.table-title {
-  color: blue;
+.error {
+  color: red;
 }
 </style>
