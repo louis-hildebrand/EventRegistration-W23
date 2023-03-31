03bc89bbcad0d5d33f30ccfecdbfcd7203880aee
Louis Hildebrand <louis.hildebrand@mcgill.ca>
2023-03-31 13:42:04 -0400

    Create static component

---
+++ EventRegistration-Frontend/src/components/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="home-page">
+    <h2 class="table-title">Users</h2>
+    <table>
+      <tr>
+        <th>Name</th>
+        <th>Creation date</th>
+      </tr>
+      <tr>
+        <td>John</td>
+        <td>2023/03/31</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+td, th {
+  border: 1px solid black;
+  padding: 0.5em;
+}
+
+#home-page {
+  display: flex;
+  flex-direction: column;
+  padding: 5%;
+  align-items: stretch;
+}
+
+.table-title {
+  color: blue;
+}
+</style>
